c8121f8c3132d5b125f055d29b0435fc54ff8115
Mateusz Jerzyk <jerzykm21@gmail.com>
2022-12-06 19:52:40 +0100

    day 6

---
+++ day6/6.py
@@ -0,0 +1,21 @@
+def get_marker_pos(code, n_characters):
+    q = []
+    for i, letter in enumerate(list(code), start=1):
+        q.append(letter)
+
+        if len(q) > 3 and len(q[-n_characters:]) == len(set(q[-n_characters:])):
+            return i
+
+    return None
+
+
+if __name__ == '__main__':
+
+    with open('data.txt', 'r') as f:
+        code = f.read()
+
+    marker_pos = get_marker_pos(code, 4)
+    print(marker_pos)
+
+    start_message_marker = get_marker_pos(code, 14)
+    print(start_message_marker)
